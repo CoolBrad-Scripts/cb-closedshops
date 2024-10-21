1ac595b80c86e0c1b6804ffc36a3dbc4513848ee
SwagDaddyBrad <bradlee.kolvoord@gmail.com>
2024-10-21 00:18:23 -0400

    Initial commit

---
+++ client/main.lua
@@ -0,0 +1,227 @@
+ClosedShopPeds = {}
+function AddStockMenu(job)
+    local menuOptions = {}
+
+    -- Find the allowed items based on the player's job
+    local allowedItems = {}
+    for _, shop in pairs(Config.ClosedShops) do
+        if shop.job == job then
+            allowedItems = shop.allowedItems
+            break
+        end
+    end
+
+    for _, item in ipairs(allowedItems) do
+        table.insert(menuOptions, {
+            title = GetItemLabel(item),
+            description = "Add " .. GetItemLabel(item) .. " to the shop",
+            icon = GetItemImage(item),
+            arrow = true,
+            disabled = not HasItemClient(item, 1),
+            onSelect = function()
+                local maxAmount = 1000
+                if UsingOxInventory then
+                    maxAmount = exports.ox_inventory:GetItemCount(item)
+                end
+                local amount = lib.inputDialog('Add Stock', {
+                    {type = 'number', label = 'Amount', description = 'Enter the amount of items to add', required = true, min = 1, max = maxAmount},
+                })
+                local price = lib.inputDialog('Add Stock', {
+                    {type = 'number', label = 'Price', description = 'Enter the Price', required = true, min = 1, max = 10000},
+                })
+                local addedToStock = lib.callback.await('cb-closedshops:server:AddStock', false, item, amount[1], price[1])
+                if addedToStock then
+                    Notify("Success", "You have added " .. amount[1] .. " " .. GetItemLabel(item) .. " to the shop!", "success")
+                else
+                    Notify("Error", "Failed to add " .. amount[1] .. " " .. GetItemLabel(item) .. " to the shop!", "error")
+                end
+            end
+        })
+    end
+
+    lib.registerContext({
+        id = 'AddStockMenu',
+        title = "Add Stock",
+        options = menuOptions
+    })
+    lib.showContext('AddStockMenu')
+end
+
+function RemoveStockMenu(job)
+    local menuOptions = {}
+    
+    -- Fetch stock items from the server
+    local stockItems = lib.callback.await('cb-closedshops:server:GetStockItems', false, job)
+    
+    -- If no stock items, exit the function
+    if not stockItems or #stockItems == 0 then
+        Notify("Error", "No items found in stock!", "error")
+        return
+    end
+    
+    -- Iterate over the stock items and build menu options
+    for k, v in ipairs(stockItems) do
+        table.insert(menuOptions, {
+            title = GetItemLabel(v.item),  -- Use the item label
+            description = "Remove " .. GetItemLabel(v.item) .. " from the shop",
+            icon = GetItemImage(v.item),  -- Assume GetItemImage returns a valid icon
+            arrow = true,
+            disabled = v.amount <= 0,  -- Disable if no stock
+            onSelect = function()
+                local maxAmount = v.amount
+                
+                -- Display input dialog for removing stock
+                local amount = lib.inputDialog('Remove Stock', {
+                    {type = 'number', label = 'Amount', description = 'Enter the amount of items to remove', required = true, min = 1, max = maxAmount},
+                })
+                
+                if amount then
+                    -- Call the server-side removal function
+                    local removedFromStock = lib.callback.await('cb-closedshops:server:RemoveStock', false, v.item, amount[1])
+                    
+                    -- Handle the result of the stock removal
+                    if removedFromStock then
+                        Notify("Success", "You have removed " .. amount[1] .. " " .. GetItemLabel(v.item) .. " from the shop!", "success")
+                    else
+                        Notify("Error", "Failed to remove " .. amount[1] .. " " .. GetItemLabel(v.item) .. " from the shop!", "error")
+                    end
+                end
+            end
+        })
+    end
+    
+    -- Register and display the menu
+    lib.registerContext({
+        id = 'RemoveStockMenu',
+        title = GetPlayerJobLabel() .. " - Remove Items",
+        options = menuOptions
+    })
+    
+    -- Show the menu
+    lib.showContext('RemoveStockMenu')
+end
+
+function OpenShopMenu(job)
+    local menuOptions = {
+        {
+            title = "Add Stock",
+            description = "Add items to the shop",
+            icon = "fa-solid fa-boxes-stacked",
+            iconColor = "green",
+            arrow = true,
+            onSelect = function()
+                AddStockMenu(job)
+            end
+        },
+        {
+            title = "Remove Stock",
+            description = "Remove items from the shop",
+            icon = "fa-solid fa-boxes-stacked",
+            iconColor = "red",
+            arrow = true,
+            onSelect = function()
+                RemoveStockMenu(job)
+            end
+        }
+    }
+
+    lib.registerContext({
+        id = 'OpenShopMenu',
+        title = GetPlayerJobLabel(),
+        options = menuOptions
+    })
+    lib.showContext('OpenShopMenu')
+end
+
+local function spawnClosedShopPedForPlayer(job)
+    local closedShopModel = `a_m_y_business_02`
+    
+    -- Load the model
+    RequestModel(closedShopModel)
+    local tries = 0
+    while not HasModelLoaded(closedShopModel) and tries < 10 do
+        Wait(500)
+        tries = tries + 1
+    end
+
+    if HasModelLoaded(closedShopModel) then
+        -- Find the shop configuration for the given job
+        local shopData = nil
+        for _, shop in pairs(Config.ClosedShops) do
+            if job == shop.job then
+                shopData = shop
+                break
+            end
+        end
+
+        if shopData and shopData.coords then
+            local coords = shopData.coords
+            local closedShopPed = CreatePed(5, closedShopModel, coords.x, coords.y, coords.z, coords.w, true, true)
+            
+            if DoesEntityExist(closedShopPed) then
+                FreezeEntityPosition(closedShopPed, true)
+                SetEntityInvincible(closedShopPed, true)
+                Wait(100)
+                TaskStartScenarioInPlace(closedShopPed, "WORLD_HUMAN_CLIPBOARD", 0, true)
+                if not ClosedShopPeds[job] then
+                    ClosedShopPeds[job] = {}
+                end
+                table.insert(ClosedShopPeds[job], closedShopPed)
+
+                exports.ox_target:addLocalEntity(closedShopPed, {
+                    {
+                        label = shopData.label,
+                        icon = "fa-solid fa-shopping-cart",
+                        distance = shopData.targetDistance,
+                        onSelect = function()
+                            exports.ox_inventory:openInventory('shop', { type = job, id = 1 })
+                        end,
+                    },
+                    {
+                        label = "Manage Shop",
+                        icon = "fa-solid fa-shopping-cart",
+                        distance = shopData.targetDistance,
+                        onSelect = function()
+                            OpenShopMenu(job)
+                        end,
+                        canInteract = function()
+                            local PlayerData = GetPlayerData()
+                            local playerJob = PlayerData.job.name
+                            return (ClosedShopPeds[playerJob] ~= nil) and (playerJob == job)
+                        end
+                    },
+                })
+            else
+                lib.print.error("Failed to create the shop ped at " .. tostring(coords))
+            end
+        end
+        SetModelAsNoLongerNeeded(closedShopModel)
+    end
+end
+
+RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
+    TriggerServerEvent('cb-closedshops:server:OnLoadSpawnShopPeds')
+end)
+
+RegisterNetEvent('cb-closedshops:client:SpawnClosedShopPed')
+AddEventHandler('cb-closedshops:client:SpawnClosedShopPed', function(job)
+    spawnClosedShopPedForPlayer(job)
+end)
+
+RegisterNetEvent('cb-closedshops:client:DeleteClosedShopPed')
+AddEventHandler('cb-closedshops:client:DeleteClosedShopPed', function(job)
+    if ClosedShopPeds[job] then
+        for _, ped in ipairs(ClosedShopPeds[job]) do
+            if DoesEntityExist(ped) then
+                TriggerServerEvent('cb-closedshops:server:DeletePed', ped)
+            end
+        end
+        ClosedShopPeds[job] = nil -- Clear the table for this job
+    end
+end)
+
+RegisterNetEvent('cb-closedshops:client:DeletePed', function(ped)
+    if DoesEntityExist(ped) then
+        DeleteEntity(ped)
+    end
+end)
---
+++ server/main.lua
@@ -0,0 +1,291 @@
+SpawnedShopPeds = {}
+
+AddEventHandler('QBCore:Server:SetDuty', function(source, onDuty)
+    local src = source
+    local Player = GetPlayer(src)
+    local job = Player.PlayerData.job.name
+    -- Loop through all closed shops in the config
+    for _, shop in pairs(Config.ClosedShops) do
+        if job == shop.job then
+            if GetDutyCount(job) == 0 then
+                -- Spawn the shop ped if no one is on duty
+                TriggerClientEvent('cb-closedshops:client:SpawnClosedShopPed', -1, job)
+                SpawnedShopPeds[job] = true
+            else
+                -- Delete the shop ped if someone is on duty
+                if (SpawnedShopPeds[job] ~= nil) then
+                    TriggerClientEvent('cb-closedshops:client:DeleteClosedShopPed', -1, job)
+                    SpawnedShopPeds[job] = false
+                end
+            end
+        end
+    end
+end)
+
+RegisterNetEvent('cb-closedshops:server:OnLoadSpawnShopPeds')
+AddEventHandler('cb-closedshops:server:OnLoadSpawnShopPeds', function()
+    for _, shop in pairs(Config.ClosedShops) do
+        local onDuty = GetDutyCount(shop.job)
+        print(onDuty, shop.job)
+        if onDuty <= 0 then
+            TriggerClientEvent('cb-closedshops:client:SpawnClosedShopPed', -1, shop.job)
+            SpawnedShopPeds[shop.job] = true
+            UpdateClosedShop(shop.job)
+        end
+    end
+end)
+
+AddEventHandler('onResourceStart', function(resourceName)
+    if (GetCurrentResourceName() ~= resourceName) then
+        return
+    end
+    -- Loop through each closed shop in the config
+    for _, shop in pairs(Config.ClosedShops) do
+        local onDuty = GetDutyCount(shop.job)
+        if onDuty == nil then onDuty = 0 end        
+        -- If no one is on duty for the job related to this shop, spawn the ped
+        if onDuty == 0 then
+            Citizen.Wait(1500)
+            TriggerClientEvent('cb-closedshops:client:SpawnClosedShopPed', -1, shop.job)
+            SpawnedShopPeds[shop.job] = true
+        end
+    end
+
+    -- Call any other necessary update functions after spawning the peds
+    for _, shop in pairs(Config.ClosedShops) do
+        UpdateClosedShop(shop.job)
+    end
+end)
+
+CreateThread(function()
+    if UsingOxInventory then
+        local hookId = exports.ox_inventory:registerHook('buyItem', function(payload)
+            for _, shop in pairs(Config.ClosedShops) do
+                if payload.shopType == shop.job then
+                    local item = payload.itemName
+                    local count = payload.count
+                    if RemoveFromStock(item, count, shop.job) then
+                        local price = payload.totalPrice
+                        local Player = GetPlayer(payload.source)
+                        if Player == nil then return end
+                        local fullName = Player.PlayerData.charinfo.firstname .. ' ' .. Player.PlayerData.charinfo.lastname
+                        exports['Renewed-Banking']:handleTransaction(payload.job, "Payment from Citizen", payload.totalPrice, "Payment from " .. fullName, fullName, "Vanilla Unicorn", "deposit")
+                        exports['Renewed-Banking']:addAccountMoney(payload.job, payload.totalPrice)
+                        return true
+                    else
+                        return false
+                    end
+                else
+                    return true
+                end
+            end
+        end, {})
+    end
+end)
+
+lib.callback.register('cb-closedshops:server:GetStockItems', function(source, job)
+    local src = source
+    if src == nil then return false end
+    local Player = GetPlayer(src)
+    if Player == nil then return end
+
+    local query = [[
+        SELECT item, price, amount FROM business_stock WHERE business = ?
+    ]]
+    local result = SQLQuery(query, {job})
+    if result and #result > 0 then
+        return result
+    else
+        return false
+    end
+end)
+
+lib.callback.register('cb-closedshops:server:hasRequiredItem', function(source, item)
+    local src = source
+    if src == nil then return false end
+    local Player = GetPlayer(src)
+    if Player == nil then return end
+    if HasItem(src, item, 1) then
+        return true
+    else
+        return false
+    end
+end)
+
+lib.callback.register('cb-closedshops:server:AddStock', function(source, item, amount, price)
+    local src = source
+    if src == nil then return false end
+    local Player = GetPlayer(src)
+    if Player == nil then return end
+    local job = Player.PlayerData.job.name
+    if Player == nil then return false end
+
+    -- Remove item from player's inventory (if applicable)
+    if RemoveItem(src, item, amount) then
+        -- Check if the item already exists for the given business in the database
+        local checkQuery = [[
+            SELECT amount FROM business_stock WHERE business = ? AND item = ?
+        ]]
+        local result = SQLQuery(checkQuery, {job, item})
+
+        if result and #result > 0 then
+            -- If the item exists, update the amount
+            local updateQuery = [[
+                UPDATE business_stock 
+                SET amount = amount + ?, price = ?, updated_at = CURRENT_TIMESTAMP
+                WHERE business = ? AND item = ?
+            ]]
+            SQLQuery(updateQuery, {amount, price, job, item})
+            UpdateClosedShop(job)
+        else
+            -- If the item does not exist, insert a new row
+            local insertQuery = [[
+                INSERT INTO business_stock (business, item, amount, price, updated_at)
+                VALUES (?, ?, ?, ?, CURRENT_TIMESTAMP)
+            ]]
+            SQLQuery(insertQuery, {job, item, amount, price})
+            UpdateClosedShop(job)
+        end
+        return true
+    else
+        return false
+    end
+end)
+
+lib.callback.register('cb-closedshops:server:RemoveStock', function(source, item, amount)
+    local src = source
+    if src == nil then return false end
+
+    local Player = GetPlayer(src)
+    if Player == nil then return false end
+
+    local job = Player.PlayerData.job.name
+
+    if UsingOxInventory then
+        -- Query to get the current amount of the item in stock for the player's job/business
+        local query = [[
+            SELECT amount FROM business_stock WHERE business = ? AND item = ?
+        ]]
+        local result = SQLQuery(query, {job, item})
+
+        if result and #result > 0 then
+            local currentAmount = result[1].amount
+
+            -- Check if there's enough stock to remove
+            if currentAmount >= amount then
+                -- Update the stock, reducing the item count
+                local updateQuery = [[
+                    UPDATE business_stock SET amount = amount - ? WHERE business = ? AND item = ?
+                ]]
+                local updateResult = SQLQuery(updateQuery, {amount, job, item})
+
+                -- If update is successful, return true
+                if updateResult then
+                    if AddItem(source, item, amount) then
+                        UpdateClosedShop(job)
+                        return true
+                    else
+                        TriggerClientEvent('cb-closedshops:client:Notify', source, "Inventory Error", "There was an issue adding the item! Please try again!", "error")
+                        return false
+                    end
+                else
+                    return false
+                end
+            else
+                -- Not enough stock to remove the requested amount
+                TriggerClientEvent('cb-closedshops:client:Notify', source, "Not Enough Stock", "Not enough stock of " .. GetItemLabel(item) .. " to remove.", "error")
+                return false
+            end
+        else
+            -- Item not found in stock
+            TriggerClientEvent('cb-closedshops:client:Notify', source, "No Stock", "You don't have any of this item in stock!", "error")
+            return false
+        end
+    else
+        return false
+    end
+end)
+
+function RemoveFromStock(item, amount, job)
+    if UsingOxInventory then
+        -- Query to get the current amount of the item in stock
+        local query = [[
+            SELECT amount FROM business_stock WHERE business = ? AND item = ?
+        ]]
+        local result = SQLQuery(query, {job, item})
+
+        if result and #result > 0 then
+            local currentAmount = result[1].amount
+
+            -- Check if there's enough stock to remove
+            if currentAmount >= amount then
+                -- Update the stock, reducing the item count
+                local updateQuery = [[
+                    UPDATE business_stock SET amount = amount - ? WHERE business = ? AND item = ?
+                ]]
+                local updateResult = SQLQuery(updateQuery, {amount, job, item})
+
+                -- If update is successful, return true
+                if updateResult then
+                    return true
+                else
+                    -- Handle failure of stock update
+                    return false
+                end
+            else
+                -- Not enough stock to remove the requested amount
+                return false
+            end
+        else
+            -- Item not found in stock
+            return false
+        end
+    else
+        -- OxInventory is not in use, handle accordingly
+        return false
+    end
+end
+
+function UpdateClosedShop(job)
+    if UsingOxInventory then
+        -- Query to get the items and prices from the database for the specified job
+        local query = [[
+            SELECT item, price, amount FROM business_stock WHERE business = ?
+        ]]
+        local result = SQLQuery(query, {job})  -- Use the passed job to query
+
+        -- Prepare the inventory dynamically based on the result from the database
+        local inventory = {}
+
+        if result and #result > 0 then
+            for i = 1, #result do
+                table.insert(inventory, {
+                    name = result[i].item,
+                    price = result[i].price,
+                    count = result[i].amount,
+                    currency = 'money'
+                })
+            end
+        end
+
+        -- Register the shop with the dynamic inventory
+        for _, shop in pairs(Config.ClosedShops) do
+            if shop.job == job then
+                local uniquename = shop.job
+                exports.ox_inventory:RegisterShop(uniquename, {
+                    name = shop.job,
+                    inventory = inventory,
+                    locations = {
+                        vec3(shop.coords.x, shop.coords.y, shop.coords.z)
+                    }
+                })
+            end
+        end
+    end
+end
+
+RegisterNetEvent('cb-closedshops:server:DeletePed')
+AddEventHandler('cb-closedshops:server:DeletePed', function(ped)
+    print(ped)
+    TriggerClientEvent('cb-closedshops:client:DeletePed', -1, ped)
+end)
